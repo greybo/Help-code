a0b2fabf2d46a5adcd72e75127fa93ac021d62d5
greybo <sergeybotl@gmail.com>
2022-10-06 10:33:55 +0300

    navigation

--- app/src/main/java/com/example/help_code/start/MainRouter.kt
+++ app/src/main/java/com/example/help_code/start/MainRouter.kt
@@ -1,15 +1,18 @@
 package com.example.help_code.start
 
+import androidx.core.os.bundleOf
 import androidx.navigation.NavController
 import com.example.help_code.R
-import com.example.help_code.base.CodeHelpRoute
 
-class MainRouter(private val controller: NavController): CodeHelpRoute() {
+class MainRouter(private val controller: NavController) {
 
     fun navigation(name: String) {
         when (FragmentName.values().find { it.rawValue == name }) {
-            FragmentName.DropDown -> controller.navigate(R.id.dropDownListFragment)
-            FragmentName.PagerFragment -> controller.navigate(R.id.toPagerMainFragment)
+            FragmentName.DropDown -> controller.navigate(R.id.toDropDownListFragment)
+            FragmentName.PagerFragment -> controller.navigate(
+                R.id.toPagerMainFragment,
+                bundleOf("tab_number" to "2")
+            )
             else -> {}
         }
     }
