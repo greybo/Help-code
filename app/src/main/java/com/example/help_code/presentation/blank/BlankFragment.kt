73ea54bd9425bb7dadb75a9afd84a9bffc9e9d6d
greybo <sergeybotl@gmail.com>
2023-11-02 14:34:52 +0200

    coroutine 2

--- app/src/main/java/com/example/help_code/presentation/blank/BlankFragment.kt
+++ app/src/main/java/com/example/help_code/presentation/blank/BlankFragment.kt
@@ -5,15 +5,6 @@ import android.view.View
 import androidx.fragment.app.viewModels
 import com.example.help_code.base.BaseBindingFragment
 import com.example.help_code.databinding.FragmentBlankBinding
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
-import kotlinx.coroutines.awaitAll
-import kotlinx.coroutines.flow.asFlow
-import kotlinx.coroutines.flow.flowOf
-import kotlinx.coroutines.flow.zip
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withContext
-import timber.log.Timber
 
 class BlankFragment : BaseBindingFragment<FragmentBlankBinding>(FragmentBlankBinding::inflate) {
 
@@ -21,45 +12,10 @@ class BlankFragment : BaseBindingFragment<FragmentBlankBinding>(FragmentBlankBin
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-//        test2()
-        viewModel.test3()
+        viewModel.mainTest()
+
 //        binding.blankFragmentButton.setOnClickListener { }
     }
 
-    val list = listOf(
-        Pair("one", 4000L),
-        Pair("two", 5000L),
-        Pair("three", 100L),
-    )
 
-    fun test2() = runBlocking<Unit> {
-        val map = list.map {
-            async { runTask(it.first, it.second) }
-        }.awaitAll()
-        Timber.i("index -> ${map}")
-    }
-
-    fun test() = runBlocking<Unit> {
-        val nums = (1..3).asFlow() // numbers 1..3
-        val strs = flowOf(
-            async { runTask("one", 1000) },//.await(),
-            async { runTask("two", 2000) },//.await(),
-            async { runTask("three", 100) },//.await()
-        ) // strings
-        nums.zip(strs) { index, data ->
-            Timber.i("$index -> ${data.await()}")
-            index != 3
-        } // compose a single string
-            .collect {
-                Timber.i(it.toString())
-            } // collect and print
-    }
-
-    private suspend fun runTask(s: String, time: Long): String {
-        return withContext(Dispatchers.IO) {
-            Timber.i("start: $s")
-            kotlinx.coroutines.delay(time)
-            s
-        }
-    }
 }
--- app/src/main/java/com/example/help_code/presentation/blank/BlankViewModel.kt
+++ app/src/main/java/com/example/help_code/presentation/blank/BlankViewModel.kt
@@ -2,10 +2,12 @@ package com.example.help_code.presentation.blank
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.CoroutineExceptionHandler
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.zip
 import timber.log.Timber
+import java.net.HttpRetryException
 
 class BlankViewModel : ViewModel() {
     // TODO: Implement the ViewModel
@@ -13,29 +15,136 @@ class BlankViewModel : ViewModel() {
         Timber.e(error)
     }
 
-    fun test3() {
-        viewModelScope.launch(handleError) {
-            runTask()
-//            kotlin.runCatching {
-////                withContext(Dispatchers.IO) {
-//
-//                delay(1000)
-//                throw HttpRetryException("test exception", 400)
-////                }
-//
-//            }.onSuccess {
-//                // do something with success response
-//                Timber.d("success call")
-//            }.onFailure {
-//                Timber.e(it)
-//            }
+    fun mainTest() {
+        viewModelScope.launch {
+            Timber.d("Start")
+            test()
+//            runCatchingTest()
+//            doWorld2()
 
+            Timber.d("Done")
         }
     }
 
-    suspend fun runTask():Boolean {
-        delay(1000)
-//        throw HttpRetryException("test exception", 400)
-        throw Throwable("test exception" )
+    fun test2() = runBlocking<Unit> {
+        val map = list.map {
+            async { runTask(it.first, it.second) }
+        }.awaitAll()
+        Timber.i("index -> ${map}")
     }
+
+    fun test() = runBlocking<Unit> {
+        val nums = (1..3).asFlow() // numbers 1..3
+        val strs = flowOf(
+            async { runTask("one", 1000) },//.await(),
+            async { runTaskException("two", 2000) },//.await(),
+            async { runTask("three", 100) },//.await()
+        ) // strings
+        nums.zip(strs) { index, data ->
+            Timber.i("$index -> ${data.await()}")
+            index != 3
+        } // compose a single string
+            .collect {
+                Timber.i(it.toString())
+            } // collect and print
+    }
+
+    //    private suspend fun runTask(s: String, time: Long): String {
+//        return withContext(Dispatchers.IO) {
+//            Timber.i("start: $s")
+//            kotlinx.coroutines.delay(time)
+//            s
+//        }
+//    }
+    suspend fun runTasksWithException() = coroutineScope { // this: CoroutineScope
+        Timber.w("Must complete 3 tasks with exception")
+        launch { runTask("task 1", 3000) }
+        try {
+            launch { runTaskException("task 2", 4000) }
+        } catch (e: Exception) {
+            Timber.e("try with exception:", e)
+        }
+        launch { runTask("task 3", 5000) }
+    }
+
+    suspend fun doTasks() = coroutineScope { // this: CoroutineScope
+        Timber.w("Must complete 2 tasks with first exception")
+        async {
+            runTaskException("Task 1", 5000L)
+        }.await()
+        async {
+            runTask("Task 2", 1000L)
+        }.await()
+        Timber.d("Hello")
+    }
+
+
+    suspend fun runCatchingTest() = runBlocking {
+
+        kotlin.runCatching {
+
+            listTasks.map {
+                async {
+                    it.run()
+                }
+            }.awaitAll().all { it }
+
+//            runTasksWithException()
+//            async {
+//                runTaskException("Task 1", 5000L)
+//            }.await()
+
+        }.onSuccess {
+            // do something with success response
+            Timber.d("success call: $it")
+        }.onFailure {
+            Timber.e("runCatching: $it")
+        }
+    }
+}
+
+val listFlow = flowOf(
+    RunTask.Simple("Task 1", 5000L),
+    RunTask.Exception("Task 2", 3000L),
+    RunTask.Simple("Task 3", 5000L),
+)
+val listTasks = listOf(
+    RunTask.Simple("Task 1", 5000L),
+    RunTask.Exception("Task 2", 3000L),
+    RunTask.Simple("Task 3", 5000L),
+)
+val list = listOf(
+    Pair("one", 4000L),
+    Pair("two", 5000L),
+    Pair("three", 100L),
+)
+
+sealed class RunTask() {
+    abstract suspend fun run(): Boolean
+
+    class Simple(private val message: String, private val delay: Long) : RunTask() {
+        override suspend fun run(): Boolean {
+            runTask(message, delay)
+            return true
+        }
+    }
+
+    class Exception(private val message: String, private val delay: Long) : RunTask() {
+        override suspend fun run(): Boolean {
+            runTaskException(message, delay)
+            return true
+        }
+    }
+}
+
+suspend fun runTask(message: String, delay: Long = 1000): Boolean {
+    delay(delay)
+    Timber.d(message)
+    return true
+}
+
+suspend fun runTaskException(message: String, delay: Long = 1000): Boolean {
+    delay(delay)
+    throw HttpRetryException("test exception: ${message}", 400)
+//        throw Throwable("test exception")
 }
