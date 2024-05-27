aa8f2eb138323de8c6e7f543068f380a0c16c693
SBotlevskyi <sergeybotl@gmail.com>
2024-05-27 15:40:09 +0300

    scan loading module temp 3

--- app/src/main/java/com/example/help_code/presentation/scannergms/GmsBarcodeScannerFragment.kt
+++ app/src/main/java/com/example/help_code/presentation/scannergms/GmsBarcodeScannerFragment.kt
@@ -2,37 +2,42 @@ package com.example.help_code.presentation.scannergms
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
-import androidx.navigation.fragment.findNavController
 import com.example.help_code.base.BaseBindingFragment
 import com.example.help_code.databinding.FragmentGmsBarcodeScanningBinding
-import com.example.help_code.permission.cameraPermissionLaunch
-import com.example.help_code.permission.isPermissionCamera
 
 class GmsBarcodeScannerFragment :
     BaseBindingFragment<FragmentGmsBarcodeScanningBinding>(FragmentGmsBarcodeScanningBinding::inflate) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+//        startCamera()
+        scanner.onScanButtonClicked()
+//        if (isPermissionCamera) {
+//            startCamera()
+//        } else cameraPermissionLaunch {
+//            if (it) {
+//                startCamera()
+//            } else {
+//                Toast.makeText(requireContext(), "Camera Permission Denied", Toast.LENGTH_SHORT)
+//                    .show()
+//                findNavController().popBackStack()
+//            }
+//        }
+    }
 
-        if (isPermissionCamera) {
-            startCamera()
-        } else cameraPermissionLaunch {
-            if (it) {
-                startCamera()
-            } else {
-                Toast.makeText(requireContext(), "Camera Permission Denied", Toast.LENGTH_SHORT)
-                    .show()
-                findNavController().popBackStack()
-            }
+    val scanner by lazy { GmsScanner(requireContext()) }
+    private fun startCamera() {
+
+        binding.gmsScanButton.setOnClickListener {
+            scanner.onScanButtonClicked()
+//            bindBarcodeScanning(requireContext()) { barcode ->
+//                binding.gmsScanText.text = barcode
+//            }
         }
     }
 
-    private fun startCamera() {
-        binding.gmsScanButton.setOnClickListener {
-            bindBarcodeScanning(requireContext()) { barcode ->
-                binding.gmsScanText.text = barcode
-            }
-        }
+    override fun onDestroyView() {
+        scanner.cancel()
+        super.onDestroyView()
     }
 }
